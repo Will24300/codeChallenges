f2780b20c3f7ee0c498cd0df906ebdef6bf2ab68
volonte rwicha <volonterwicha2004@gmail.com>
2025-10-08 11:31:31 +0200

    Break camel case

--- breakcamelCase.js
+++ breakcamelCase.js
@@ -1,20 +1,15 @@
 function solution(string) {
-  const upper = string.toUpperCase().split("");
-  const filtered = upper.filter((item) => string.includes(item));
-  const newArr = [...new Set(filtered)];
-  let str = [];
+  let str = "";
 
-  for (const letter of newArr) {
-    if (string.includes(letter)) {
-      str.push(string.replace(letter, ` ${letter}`));
+  for (const char of string) {
+    if (char === char.toUpperCase()) {
+      str += " " + char;
+    } else {
+      str += char;
     }
   }
-  if (solution(str[0])) {
-    return str;
-  } else {
-    return str;
-  }
+
+  return str;
 }
 
-const result = solution("camelCasingTest");
-console.log(result);
+const result = solution("camel");
