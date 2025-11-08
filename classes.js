c1b6c10904920f129eb4eda6aae8bf686286d2fc
Volonte Rwicha <volonterwicha2004@gmail.com>
2025-11-09 00:48:47 +0200

    test

--- classes.js
+++ classes.js
@@ -1,7 +1,12 @@
-const gameSettings = {};
-const gameSettingsProxy = new Proxy(gameSettings, {
-  get: (o, prop) => {
-    return prop in o ? "Yes" : "Nop";
-  },
-});
-console.log(gameSettingsProxy.difficulty);
+function minimumBillCount(value, availables) {
+  const numbers = [];
+  for (const num of availables) {
+    numbers.push(value / num);
+  }
+
+  return numbers.sort((a, b) => a - b)[0];
+}
+console.log(minimumBillCount(10000000, [1, 10, 20, 50, 100]));
+// console.log(minimumBillCount(1010, [50, 1, 20]));
+console.log(minimumBillCount(1234567, [1, 10, 20]));
+console.log(minimumBillCount(40, [1, 10, 20]));
