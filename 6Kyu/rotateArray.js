fb08075775265adf3fc6ccdc3150b2f82825d768
Volonte Rwicha <133901065+Will24300@users.noreply.github.com>
2025-11-24 17:56:56 +0200

    rotate array

---
+++ 6Kyu/rotateArray.js
@@ -0,0 +1,16 @@
+function rotate(data, n){
+    if (n === 0 || data.length === 0) return data;
+    if (n > 0){
+        for (let i = 0; i < n; i++){
+            const poped = data.pop();
+            data.unshift(poped);
+        }
+            return data;
+    } else {
+        for (let i = 0; i < Math.abs(n); i++){
+            const shifted = data.shift();
+            data.push(shifted);
+        }
+        return data;
+    }
+}
