f1b8bc0ba64e48cd7bd39e69878522da0093cde9
Volonte Rwicha <133901065+Will24300@users.noreply.github.com>
2025-12-26 23:50:24 +0200

    changes

--- classes.js
+++ classes.js
@@ -1,10 +1,10 @@
 function countRepeats(str) {
-    const res = [];
-    for (let i = 0; i < str.length; i++){
-        if (str.indexOf(str[i]) !== str.lastIndexOf(str[i])){
-            res.push(str[i])
-        }
-    }
-    const minus = [...new Set(res)].length;
-    return res.length - minus;
+    // const res = [];
+    // for (let i = 0; i < str.length; i++){
+    //     if (str.indexOf(str[i]) !== str.lastIndexOf(str[i])){
+    //         res.push(str[i])
+    //     }
+    // }
+    // const minus = [...new Set(res)].length;
+    // return res.length - minus;
 }
