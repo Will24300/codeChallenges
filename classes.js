d61cb1176ac183c561b375aa8a4ff8ff5be459b2
Volonte Rwicha <133901065+Will24300@users.noreply.github.com>
2026-01-28 15:37:55 +0200

    Reverse and invert not done yet 7kyu

--- classes.js
+++ classes.js
@@ -1,7 +1,18 @@
-function incrementer(nums) { 
-  const arr = [];
-  for (let i = 1; i <= nums.length; i++){
-      arr.push(i + nums[i - 1]);
+function reverseInvert(array) {
+  const filter = array.filter((n) => Number.isInteger(n));
+  const abs = filter.map((num) => -num);
+  const res = [];
+  for (const ch of abs) {
+    if (String(ch).length > 2) {
+      const str = String(ch).split("").reverse().join("");
+      if (str.endsWith("-")) {
+        const cut = str.slice(0, str.length - 1);
+        res.push(-Number(cut));
+      }
+    }
+    res.push(ch);
   }
-  return arr;
+  console.log(res);
 }
+
+reverseInvert([1, 12, "a", 3.4, 87, 99.9, -42, 50, 5.6]); // [-1,-21,-78,24,-5]
