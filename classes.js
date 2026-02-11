b75715ee374453f1c2f071fb690f3e605b9de845
Volonte Rwicha <133901065+Will24300@users.noreply.github.com>
2026-02-11 21:32:56 +0200

    returning res

--- classes.js
+++ classes.js
@@ -12,7 +12,7 @@ function reverseInvert(array) {
     }
     res.push(ch);
   }
-  console.log(res);
+  return res;
 }
 
 reverseInvert([1, 12, "a", 3.4, 87, 99.9, -42, 50, 5.6]); // [-1,-21,-78,24,-5]
