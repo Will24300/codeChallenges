0066f1ade32b86afef9739a8498bb13231bb04b9
Volonte Rwicha <volonterwicha2004@gmail.com>
2026-05-25 14:35:57 +0200

    add uniquer in order

---
+++ 6Kyu/uniqueInOrder.js
@@ -0,0 +1,15 @@
+var uniqueInOrder = function (iterable) {
+  const myArr = typeof iterable === "object" ? iterable : iterable.split("");
+  const result = [];
+  for (let i = 0; i < myArr.length; i++) {
+    if (myArr[i] !== myArr[i + 1]) {
+      result.push(myArr[i]);
+    }
+  }
+  return result;
+//   console.log(result);
+  
+};
+
+uniqueInOrder("AAAABBBCCDAABBB");
+uniqueInOrder([1, 2, 2, 3, 3]);
